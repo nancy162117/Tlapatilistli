a3b0947d62cf4b333d130714b329b514592df11b
Nancy Citlali Martínez Torres <70962912+nancy162117@users.noreply.github.com>
2022-10-25 18:16:12 -0500

    Delete AuthContext.js

--- src/AuthContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const AuthContext = createContext({
-    auth: undefined,
-    login: () => null,
-    logout: () => null,
-});
-
-export default AuthContext;
